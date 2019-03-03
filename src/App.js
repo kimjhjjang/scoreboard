e54e563a9c13ccc72b6c87e10c3133d08c4d2c02
kimjhjjang <kimjhjjang@gmail.com>
2019-03-03 16:59:51 +0900

    component communication : 카운터 컴포넌트의 score를 lifting up, 증가, 감소 이벤트를 props로 내려줌.

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import './App.css';
 import {Header} from "./components/Header";
 import {Player} from "./components/Player";
 
+
 class App extends React.Component {
   state = {
     players: [
-      {name: 'LDK', id: 1},
-      {name: 'HONG', id: 2},
-      {name: 'KIM', id: 3},
-      {name: 'PARK', id: 4},
+      {name: 'LDK', score: 0, id: 1},
+      {name: 'HONG', score: 0, id: 2},
+      {name: 'KIM', score: 0, id: 3},
+      {name: 'PARK', score: 0, id: 4},
     ]
   }
 
@@ -20,15 +21,30 @@ class App extends React.Component {
     }))
   }
 
+  //증가 혹은 감소하는 핸들러 메서드
+  handleChangeScore = (id, delta) => {
+    console.log(id,delta);
+    this.setState(prevState => ({
+      players: prevState.players.map(item =>{
+        if(item.id === id){
+          item.score = item.score + delta;
+        }
+        return item;
+      })
+    }));
+  }
+
   render() {
     return (
       <div className="scoreboard">
-        <Header title="My Scoreboard" totalPlayers={1 + 10} />
+        <Header title="My Scoreboard" totalPlayers={this.state.players.length}/>
 
         {
           this.state.players.map(player =>
             <Player name={player.name} id={player.id} key={player.id}
-                    removePlayer={this.handleRemovePlayer}/>)
+                    score={player.score}
+                    removePlayer={this.handleRemovePlayer}
+                    changeScore={this.handleChangeScore}/>)
         }
       </div>
     )
--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,35 +1,20 @@
 import React from "react";
 
 
-class Counter extends React.Component {
+export class Counter extends React.Component {
   constructor(props) {
     super(props);  // 부모 속성 초기화해야됨.
-    this.state = {
-      score: 0
-    };
   }
-
 
   render() {
     return (
       <div className="counter">
-        <button className="counter-action decrement" onClick={() => this.decrementScore()}> -</button>
-        <span className="counter-score">{this.state.score}</span>
-        <button className="counter-action increment" onClick={this.incrementScore}> +</button>
+        <button className="counter-action decrement"
+                onClick={() => this.props.changeScore(this.props.id,-1)}> -</button>
+        <span className="counter-score">{this.props.score}</span>
+        <button className="counter-action increment"
+                onClick={() => this.props.changeScore(this.props.id,+1)}> +</button>
       </div>
     );
-  }
-
-  // 증가 점수
-  incrementScore = () => {
-    this.setState((prevState) => {
-      return {score: prevState.score + 1}
-    });
-  }
-  // 감소 점수
-  decrementScore = () => {
-    this.setState((prevState) => {
-      return {score: prevState.score - 1}
-    });
   }
 }
