6e6f6ef011689704e1e9426c596740358e466560
kimjhjjang <kimjhjjang@gmail.com>
2019-03-10 16:56:47 +0900

    scoreBoard에 redux 적용 2
    초기 state 생성

--- src/redux/reducers/player.js
+++ src/redux/reducers/player.js
@@ -1,3 +1,13 @@
-export const playerReducer = (state, action) =>{
+const initialState ={
+  title : 'Redux Score',
+  players: [
+    {name: 'LDK', score: 0, id: 1},
+    {name: 'HONG', score: 0, id: 2},
+    {name: 'KIM', score: 0, id: 3},
+    {name: 'PARK', score: 0, id: 4},
+  ]
+}
+
+export const playerReducer = (state = initialState, action) =>{
   return state;
 }
